68219b653d1d0df103de4b480d05629956068719
Akila Mendis <akila@oneit.com.au>
2021-03-01 14:33:03 +0530

    Using ModelSerializers

---
+++ snippets/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from snippets import views
+
+app_name = 'snippets'
+router = DefaultRouter()
+router.register('', views.SnippetViewSet)
+urlpatterns = [
+    path('', include(router.urls)),
+]
